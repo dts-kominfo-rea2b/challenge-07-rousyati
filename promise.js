912b54b14ea9af1528c4fbc2cc1b843ea84bf413
rousyati <82667388+rousyati@users.noreply.github.com>
2022-06-13 08:09:51 +0700

    Tugas Challenge Promise
    Done

--- promise.js
+++ promise.js
@@ -1,8 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+const promiseOutput = async (emosi) => {
+  let x = await promiseTheaterIXX();
+  let y = await promiseTheaterVGC();
+  let xY = x.concat(y);
+  let totalEmosi = 0;
+  xY.map(x => x.hasil === emosi && (totalEmosi += 1) )
+  return totalEmosi
+};
+// console.log()
+// promiseOutput("marah")
 module.exports = {
   promiseOutput,
 };
